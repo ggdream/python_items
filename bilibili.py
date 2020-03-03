d424ff0c8e20f09bcd31c5d73b2cba3100a14ae6
flyingAfish <flyingAfish@.github.com>
2020-03-03 21:04:25 +0800

    1、增加视频选集目录获取程序 2、视频文件名保存为视频选集目录名 3、增加下载完自动删除m4p和mp3文件程序

--- bilibili.py
+++ bilibili.py
@@ -1,16 +1,16 @@
 # coding=utf-8
-import requests, re, os
+import requests, re
 from urllib.request import quote
 from lxml import etree
 from multiprocessing import Pool
 from ffmpy3 import FFmpeg
 from prettytable import PrettyTable
+import os
 
 order, infors = 1, []
 table = PrettyTable(['序号', 'up主', '标题', '视频时长', '投稿日期', '播放量', '试看链接'])
-path = 'D:\哔哩哔哩视频'
-path_ = 'D:\哔哩哔哩视频\m4s文件集'
-
+path = 'G:\哔哩哔哩视频'
+path_ = 'G:\哔哩哔哩视频\m4s文件集'
 
 def _checkPath(path):
     if not os.path.exists(path):
@@ -26,15 +26,13 @@ def _getData(content, page):
         'Referer': 'https://www.bilibili.com/',
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36'
     }
+    html = etree.HTML(requests.get(url, headers=headers).content.decode('utf-8'))
     if page == 1:
-        responses = etree.HTML(requests.get(url, headers=headers).content.decode('utf-8')) \
-            .xpath('//*[@id="all-list"]/div[1]/div[2]/ul[2]/li')
+        responses = html.xpath('//*[@id="all-list"]/div[1]/div[2]/ul[2]/li')
         if responses == []:
-            responses = etree.HTML(requests.get(url, headers=headers).content.decode('utf-8')) \
-                .xpath('//*[@id="all-list"]/div[1]/div[2]/ul/li')
+            responses = html.xpath('//*[@id="all-list"]/div[1]/div[2]/ul/li')
     else:
-        responses = etree.HTML(requests.get(url, headers=headers).content.decode('utf-8')) \
-            .xpath('//*[@id="all-list"]/div[1]/ul/li')
+        responses = html.xpath('//*[@id="all-list"]/div[1]/ul/li')
     for response in responses:
         infors.append([str(order),  # 序号
                        response.xpath('./div/div[3]/span[4]/a/text()')[0],  # up主
@@ -49,7 +47,6 @@ def _getData(content, page):
                       .split('?')[0]])
         order += 1  # 序号增加
 
-
 def _getUrlsForPages(content, pages=1):
     global table, infors
     for page in range(1, pages + 1):
@@ -60,8 +57,15 @@ def _getUrlsForPages(content, pages=1):
     number = input('输入要下载的视频序号：').split('.')
     return [infors[int(i) - 1][-1] for i in number]
 
-
 def download(url, savePath, childPath, fileName):
+    """
+    下载并保存视频
+    :param url: string网页url
+    :param savePath: mp4文件保存目录
+    :param childPath: m4p文件与mp3文件存放目录
+    :param fileName: 视频文件名
+    :return:
+    """
     global path
     print(url)
     headers = {
@@ -82,16 +86,7 @@ def download(url, savePath, childPath, fileName):
             f.write(requests.get(urls1[0], headers=headers).content)
         with open(childPath + '\%s.mp3' % (fileName), 'wb')as f:
             f.write(requests.get(urls1[-1], headers=headers).content)
-        try:
-            ff: FFmpeg = FFmpeg(
-                executable="D:\Python\\ffmpeg\\ffmpeg-20200224-bc9b635-win64-static\\bin\\ffmpeg.exe",
-                inputs={childPath + '\%s.mp4' % (fileName): None, childPath + '\%s.mp3' % (fileName): None},
-                outputs={savePath + '\%s.mp4' % (fileName): '-c:v copy -c:a aac -strict experimental'}) # '-c:v h264 -c:a ac3'
-            print(ff, "-------------------------------------")
-            ff.run()
-        except:
-            print(fileName + "音视频合成失败！！失败日志已被屏蔽！！")
-        print(fileName + '下载完成!')
+        combineM3pFile(childPath, fileName, savePath)
     # 下载flv视频
     else:
         print(fileName + '开始下载!')
@@ -99,26 +94,56 @@ def download(url, savePath, childPath, fileName):
             f.write(requests.get(urls2[0], headers=headers).content)
         print(fileName + '下载完成!')
 
-def downloadsAll(url):
+def combineM3pFile(childPath, fileName, savePath):
+    """
+    讲m4p文件与mp3文件合成为一个mp4文件
+    :param childPath: m4p文件与mp3文件存放目录
+    :param fileName: 视频文件名
+    :param savePath: mp4文件存放目录
+    :return:
+    """
+    try:
+        ff: FFmpeg = FFmpeg(
+            executable="G:\\ProgramFiles\\Python\\ffmpeg\\ffmpeg-20200224-bc9b635-win64-static\\bin\\ffmpeg.exe",
+            inputs={childPath + '\%s.mp4' % (fileName): None, childPath + '\%s.mp3' % (fileName): None},
+            outputs={
+                savePath + '\%s.mp4' % (fileName): '-c:v copy -c:a aac -strict experimental'})  # '-c:v h264 -c:a ac3'
+        print(ff, "-------------------------------------")
+        ff.run()
+    except:
+        print(fileName + "音视频合成失败！！失败日志已被屏蔽！！")
+    print(fileName + '下载完成!')
+
+def downloadsAll(url, is_delete_m4p=False):
+    """
+    循环下载所有视频数据
+    :param url: 下游网址
+    :param is_delete_m4p: 是否删除m4p问价及目录
+    :return:
+    """
     minPage = 1
     maxPage = 1
     _checkPath(path)
     _checkPath(path_)
     hv = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36'}
-    pageTile = etree.HTML(requests.get(url, headers=hv).content.decode('utf-8'))\
-        .xpath('//*[@id="viewbox_report"]/h1/@title')[0]
+    html = etree.HTML(requests.get(url, headers=hv).content.decode('utf-8'))
+    pageTile = html.xpath('//*[@id="viewbox_report"]/h1/@title')[0]
     print("pageTile:", pageTile)
     # 创建子目录
     childPath = path_ + "\\" + pageTile
     savePath = path + "\\" + pageTile
     _checkPath(childPath)
     _checkPath(savePath)
-    multiPage = etree.HTML(requests.get(url, headers=hv).content.decode('utf-8'))\
-        .xpath('//*[@id="multi_page"]/div[2]/ul/li')
+    multiPage = html.xpath('//*[@id="multi_page"]/div[2]/ul/li')
     # multiPage不为空则说明此链接下有视频选集（即多个子视频）
     if multiPage != []:
         maxPage = len(multiPage)
     print("maxPage:", maxPage)
+
+    urlArr = url.split("?")[0].split('av')
+    aid = urlArr[len(urlArr) - 1]
+    # 解析json获取视频选集名字典{pageNum: title}
+    tileList = parseJson(aid)
     for p in range(minPage, maxPage + 1):
         _url = url + '?p=%d' %(p)
         # 如果是单视频则文件名就是父目录名
@@ -129,24 +154,66 @@ def downloadsAll(url):
             print(etree.tostring(multiPage[p - 1]))
             # 这里居然获取不到子视频集的名字，可能是js动态加载出来的，所以这里获取不到（网页源码是有的）
             # fileName = multiPage[p].xpath('./a/@title')[0]
-            fileName = str(p)
+            try:
+                fileName = tileList[p-1]['part']
+                print(fileName)
+            except Exception:
+                fileName = str(p)
         # 如果文件存在，则跳过本次下载
         if _existFile(savePath + '\%s.mp4' % (fileName)):
             continue
         if _existFile(childPath + '\%s.mp4' % (fileName)):
             continue
         download(_url, savePath, childPath, fileName)
+    if is_delete_m4p:
+        removeAll(path_)
 
-def start(content,pages):
+def parseJson(aid):
+    """
+    解析网页返回的json格式数据
+    :param aid: 网页id
+    :return: list类型数据
+    """
+    hv = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36'}
+    url = 'https://api.bilibili.com/x/player/pagelist?aid=%s&jsonp=jsonp'%(aid)
+    jsonStr: str = requests.get(url, headers=hv).content.decode('utf-8')
+    import json
+    jsonObject: dict = json.loads(jsonStr)
+    return jsonObject['data']
+
+def removeAll(path):
+    """
+    递归地删除path文件或者文件夹下所有目录和文件包括path本身
+    :param path: string路径
+    :return:
+    """
+    if os.path.exists(path):
+        if os.path.isfile(path):
+            os.remove(path)
+        else:
+            dirs = os.listdir(path)
+            if len(dirs) == 0:
+                os.removedirs(path)
+            for dir in dirs:
+                innerPath = path + '\\' + dir
+                print('正在删除：' + innerPath)
+                removeAll(innerPath)
+
+def start(content, pages, is_delete_m4p):
     urls = _getUrlsForPages(content, pages=pages)  # 获取待下载URL集
     pool = Pool(processes=4)  # 设置处理器个数
     pool.map(downloadsAll, urls)
     pool.close()
+    if is_delete_m4p:
+        removeAll(path_)
 
 if __name__ == '__main__':
     content = input('输入内容：')  # 爬取关键字
     pages = 3  # 爬取总页数
-    start(content,pages)
+    start(content, pages, True)
 
     # 测试下载程序OK
     # downloadsAll("https://www.bilibili.com/video/av75859780")
+    # downloadsAll("https://www.bilibili.com/video/av55181893", True)
+    # 'https://api.bilibili.com/x/player/pagelist?aid=55181893&jsonp=jsonp'
